f2d3fcfcee3eb32a0211bba594e3e9e56b545b53
Ramyking <raminmehrabi@Ramins-MacBook-Air.local>
2017-02-06 00:45:23 +0330

    unity multiplayer
    unity 5.5
    networking

---
+++ networktest1/Assets/script/playerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class playerController : NetworkBehaviour {
+
+	// bullet
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+		// move player
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+		//shoot bullet
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			CmdFire ();
+		}
+		
+	}
+	//make player blue for local , enemy player withe
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<MeshRenderer>().material.color = Color.blue;
+	}
+	// fire bullet function
+	[Command]
+	void CmdFire()
+	{
+		// Create the Bullet from the Bullet Prefab
+		var bullet = (GameObject)Instantiate(
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 10;
+
+		// Spawn the bullet on the Clients
+		NetworkServer.Spawn(bullet);
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 4.0f);
+	}
+
+}
